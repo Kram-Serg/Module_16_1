efc601608223bee58d57cfba13f83bb7c5279ab1
Kram-Serg <bild003@yandex.ru>
2024-12-17 08:34:53 +0300

    Update Module_16_1.py

--- Module_16_1.py
+++ Module_16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get('/')
-async def main_page() -> dict:
-    return {'message': 'Главная страница'}
+async def main_page():
+    return 'Главная страница'
 
 
 @app.get('/user/admin')
-async def admin_page() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def admin_page():
+    return 'Вы вошли как администратор'
 
 
 @app.get('/user/{user_id}')
-async def user(user_id: int) -> dict:
-    return {'message': f'Вы вошли как пользователь №{user_id}'}
+async def user(user_id: int):
+    return f'Вы вошли как пользователь №{user_id}'
 
 
 @app.get('/user/{user_name}/{user_age}')
-async def info_user(user_name: str, user_age: int) -> dict:
-    return {'message': f'Информация о пользователе. Имя: {user_name}, возраст: {user_age}'}
+async def info_user(user_name: str, user_age: int):
+    return f'Информация о пользователе. Имя: {user_name}, возраст: {user_age}'
